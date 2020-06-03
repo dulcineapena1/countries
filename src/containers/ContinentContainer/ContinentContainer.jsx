9340234ee76dc4cfbc5fb963efcf19d838f8a071
DulcineaPena1 <f.dulcinea.pena@gmail.com>
2020-06-03 13:37:23 -0500

    continent: container request countries

--- src/containers/ContinentContainer/ContinentContainer.jsx
+++ src/containers/ContinentContainer/ContinentContainer.jsx
@@ -5,6 +5,7 @@ class ContinentContainer extends Component {
   state = {
     active_tab: '',
     continent_info: null,
+    countries_in_continent_info: null,
   }
 
   componentDidMount = () => {
@@ -27,9 +28,35 @@ class ContinentContainer extends Component {
       .then(res => {
         let summary = res.query.pages[Object.keys(res.query.pages)[0]].extract;
         this.setState({ continent_info: summary });
+        this.requestCountriesInfo();
       })
   }
 
+  requestCountriesInfo = () => {
+    const countries_code = {
+      'Europe': ['EU'],
+      'Africa': ['AF'],
+      'Asia': ['AS'],
+      'Oceania': ['OC'],
+      'Americas': ['NA', 'SA']
+    }
+
+    let selected_country_code = countries_code[this.state.active_tab].join('", "');
+
+    let countries_in_continent_query = `{ countries(filter:{continent:{in:["${selected_country_code}"] } }) {
+  	   name emoji native languages { name } capital code currency } }`
+
+    fetch('https://countries.trevorblades.com/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({ query: countries_in_continent_query })
+    })
+      .then(res => res.json())
+      .then(res => this.setState({ countries_in_continent_info: res.data.countries }));
+  }
 
   render() {
     let main_continent_element = (
@@ -41,7 +68,8 @@ class ContinentContainer extends Component {
         <MainContinent
           onChangeTab={this.props.onChangeTab}
           activeTab={this.props.activeTab}
-          continentInfo={this.state.continent_info}/>
+          continentInfo={this.state.continent_info}
+          countries={this.state.countries_in_continent_info}/>
       );
     }
 
