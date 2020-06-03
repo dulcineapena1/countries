fe4194160ce3b48a4400ef60df785a8684ef534e
DulcineaPena1 <f.dulcinea.pena@gmail.com>
2020-06-03 18:53:57 -0500

    continent section and tabs styles

--- src/components/Continent/ContinentNav/ContinentNav.jsx
+++ src/components/Continent/ContinentNav/ContinentNav.jsx
@@ -1,49 +1,50 @@
 import React from 'react';
+import './ContinentNav.css';
 
 const ContinentNav = (props) => {
   const { activeTab, onChangeTab } = props;
 
   let nav = (
     <ul className='nav nav-tabs'>
-      <li className='nav-item'>
+      <li className='nav-item' onClick={() => onChangeTab('Africa')}>
         <a
           className={activeTab === 'Africa' ? 'nav-link active' : 'nav-link'}
-          onClick={() => onChangeTab('Africa')}
           href='#'>
-          Africa
+          <span>Africa</span>
         </a>
+        <p className='tabText'>Africa</p>
       </li>
-      <li className='nav-item'>
+      <li className='nav-item' onClick={() => onChangeTab('Americas')}>
         <a
           className={activeTab === 'Americas' ? 'nav-link active' : 'nav-link'}
-          onClick={() => onChangeTab('Americas')}
           href='#'>
-          Americas
+          <span>Americas</span>
         </a>
+        <p className='tabText'>Americas</p>
       </li>
-      <li className='nav-item'>
+      <li className='nav-item' onClick={() => onChangeTab('Asia')}>
         <a
           className={activeTab === 'Asia' ? 'nav-link active' : 'nav-link'}
-          onClick={() => onChangeTab('Asia')}
           href='#'>
-          Asia
+          <span>Asia</span>
         </a>
+        <p className='tabText'>Asia</p>
       </li>
-      <li className='nav-item'>
+      <li className='nav-item' onClick={() => onChangeTab('Europe')}>
         <a
           className={activeTab === 'Europe' ? 'nav-link active' : 'nav-link'}
-          onClick={() => onChangeTab('Europe')}
           href='#'>
-          Europe
+          <span>Europe</span>
         </a>
+        <p className='tabText'>Europe</p>
       </li>
-      <li className='nav-item'>
+      <li className='nav-item' onClick={() => onChangeTab('Oceania')}>
         <a
           className={activeTab === 'Oceania' ? 'nav-link active' : 'nav-link'}
-          onClick={() => onChangeTab('Oceania')}
           href='#'>
-          Oceania
+          <span>Oceania</span>
         </a>
+        <p className='tabText'>Oceania</p>
       </li>
     </ul>
   );
