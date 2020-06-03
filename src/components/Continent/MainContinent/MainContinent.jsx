855683754e96c4f2e86ba90d345104e85bddb2a8
DulcineaPena1 <f.dulcinea.pena@gmail.com>
2020-06-03 13:36:36 -0500

    continent: main container add countries list

--- src/components/Continent/MainContinent/MainContinent.jsx
+++ src/components/Continent/MainContinent/MainContinent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ContinentNav from '../ContinentNav';
 import ContinentCard from '../ContinentCard';
+import ContinentCountriesList from '../ContinentCountries/ContinentCountriesList';
 
 const MainContinent = (props) => {
-  const { onChangeTab, activeTab, continentInfo } = props;
+  const { onChangeTab, activeTab, continentInfo, countries } = props;
 
   let nav = (
     <ContinentNav onChangeTab={onChangeTab} activeTab={activeTab}/>
@@ -12,6 +13,13 @@ const MainContinent = (props) => {
   let card = (
     <ContinentCard activeTab={activeTab} continentInfo={continentInfo}/>
   );
+
+  let countries_list;
+  if (countries) {
+    countries_list = (
+      <ContinentCountriesList countries={countries}/>
+    );
+  }
 
   return (
     <React.Fragment>
@@ -28,7 +36,14 @@ const MainContinent = (props) => {
                   {card}
                 </div>
                 <div className='col-4'>
-                  Search section
+                  <div className='row'>
+                    <div>
+                      Search bar
+                    </div>
+                    <div className='col-12'>
+                      {countries_list}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
